fafe20168c7761ce1580b13e2dba5a0dcf043569
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-08 09:00:41 +0800

    Update vec.js

--- src/vec.js
+++ src/vec.js
@@ -1,8 +1,21 @@
-var vec = {
-  len2(v) {
-    return Module._len2(A2P(v), 2);
-  },
-  len(v) {
-    return Module._len(A2P(v), 2);
+export default async () => {
+  const vecWasmBinaries = fetch('https://raw.githubusercontent.com/LXSMNSYC/vec-wasm/master/src/vec-wasm.wasm');
+
+  let instance;
+  if (instantiateStreaming in WebAssembly) {
+    const { m, i } = await WebAssembly.instantiateStreaming(vecWasmBinaries);
+
+    instance = i;
+  } else if (compileStreaming in WebAssembly) {
+    const m = await WebAssembly.compileStreaming(vecWasmBinaries);
+    
+    instance = await WebAssembly.instantiate(m);
+  } else if (compile in WebAssembly && instantiate in WebAssembly) {
+    const buffer = await vecWasmBinaries.arrayBuffer();
+    const m = await WebAssembly.compile(buffer);
+
+    instance = await WebAssembly.instantiate(m);
   }
+
+  return vec;
 }
