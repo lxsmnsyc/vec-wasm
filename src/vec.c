173d8a4f58007d7b07311f6ec425621487813773
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-08 09:40:48 +0800

    Update vec.c

--- src/vec.c
+++ src/vec.c
@@ -8,43 +8,43 @@
 double data[MAX_DIMENSIONS];
 
 EMSCRIPTEN_KEEPALIVE
-double v_len2(double* vec, int dim) {
+double vlen2(int dim) {
   int d = CLAMP(0, dim, MAX_DIMENSIONS);
   double result = 0;
   for (int i = 0; i < d; i++) {
-    result += vec[i] * vec[i];
+    result += data[i] * data[i];
   }
   return result;
 }
 
 EMSCRIPTEN_KEEPALIVE
-double v_len(double* vec, int dim) {
-  return sqrt(v_len2(vec, dim));
+double vlen(int dim) {
+  return sqrt(vlen2(vec, dim));
 }
 
 EMSCRIPTEN_KEEPALIVE
-void v_abs(double* vec, int dim) {
+void vabs(int dim) {
   int d = CLAMP(0, dim, MAX_DIMENSIONS);
   for (int i = 0; i < dim; i++) {
-    data[i] = fabs(vec[i]);
+    data[i] = fabs(data[i]);
   }
 }
 
 EMSCRIPTEN_KEEPALIVE
-void v_clean() {
+void vclean() {
   for (int i = 0; i < MAX_DIMENSIONS; i++) {
     data[i] = 0.0;
   }
 }
 
 EMSCRIPTEN_KEEPALIVE
-void v_save(int index, double value) {
+void vsave(int index, double value) {
   if (0 <= index && index < MAX_DIMENSIONS) {
     data[index] = value;
   }
 }
 
 EMSCRIPTEN_KEEPALIVE
-double* v_load() {
+double* vload() {
   return &data[0];
 }
