215c84fd81f19d7b8f6208fedb19270eb60eddc3
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-08 09:00:44 +0800

    Update vec.c

--- src/vec.c
+++ src/vec.c
@@ -1,16 +1,36 @@
+#include <stdlib.h>
 #include <math.h>
 #include <emscripten/emscripten.h>
 
+#define MAX_DIMENSIONS 128
+#define CLAMP(a, b, c) fmax(a, fmin(b, c))
+
+double data[MAX_DIMENSIONS];
+
 EMSCRIPTEN_KEEPALIVE
-double len2(double* vec, int dim) {
+double v_len2(double* vec, int dim) {
+  int d = CLAMP(0, dim, MAX_DIMENSIONS);
   double result = 0;
-  for (int i = 0; i < dim; i++) {
+  for (int i = 0; i < d; i++) {
     result += vec[i] * vec[i];
   }
   return result;
 }
 
 EMSCRIPTEN_KEEPALIVE
-double len(double* vec, int dim) {
-  return sqrt(len2(vec, dim));
+double v_len(double* vec, int dim) {
+  return sqrt(v_len2(vec, dim));
+}
+
+EMSCRIPTEN_KEEPALIVE
+void v_abs(double* vec, int dim) {
+  int d = CLAMP(0, dim, MAX_DIMENSIONS);
+  for (int i = 0; i < dim; i++) {
+    data[i] = fabs(vec[i]);
+  }
+}
+
+EMSCRIPTEN_KEEPALIVE
+double* v_load() {
+  return &data[0];
 }
